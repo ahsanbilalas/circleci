be857c7b3f1b7bdb92f14fc8bb087f29bbd12d43
ahsanbilalas <ahsan.bilal@arbisoft.com>
2018-01-17 16:29:18 +0500

    Adding rest framework requirment

--- circleci/circleci/settings.py
+++ circleci/circleci/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'circleci.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'circleci',
-    #     'USER': 'ahsan',
-    #     'PASSWORD': 'arbisoft',
-    #     'HOST': '',
-    #     'PORT': '5432',
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'circleci',
+        'USER': 'ahsan',
+        'PASSWORD': 'arbisoft',
+        'HOST': '',
+        'PORT': '5432',
+    }
 }
 
 
